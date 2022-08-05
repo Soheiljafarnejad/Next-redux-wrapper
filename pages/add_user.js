692e726858feb317e4cf7e8e0e08a668905ec001
soheiljafarnejad <jafarnejad.official@gmail.com>
2022-08-05 16:07:39 +0430

    Use Redux in Client Side

--- pages/add_user.js
+++ pages/add_user.js
@@ -1,5 +1,31 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addUserInfo } from "../Redux/Actions/UserActions";
+
 const AddUserClient = () => {
-  return <div></div>;
+  const [value, setValue] = useState("");
+  const user = useSelector((store) => store.user.userInfo);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <div>
+        <input
+          style={{ marginRight: "0.5rem" }}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button disabled={!value} onClick={() => dispatch(addUserInfo(value))}>
+          Add user
+        </button>
+      </div>
+
+      {user.length > 0 &&
+        user.map((item, index) => {
+          return <h2 key={index}>{item}</h2>;
+        })}
+    </div>
+  );
 };
 
 export default AddUserClient;
